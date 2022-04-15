7b7ac6db0a0a9cf8e044a182fb47591d755ccb4e
mmgithub123 <71955422+mmgithub123@users.noreply.github.com>
2022-04-15 19:55:45 +0800

    Create main.go

---
+++ main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+    "flag"
+    "fmt"
+    "log-forward/conf"
+    "log-forward/kafka"
+    "log-forward/us3"
+    "time"
+
+    "gopkg.in/ini.v1"
+)
+
+var config = new(conf.Config)
+var kafkaMsg = make(chan string)
+
+func main() {
+    var (
+        confFile    string
+        us3ConfFile string
+    )
+    flag.StringVar(&confFile, "confFile", "config.ini", "set config file")
+    flag.StringVar(&us3ConfFile, "us3ConfFile", "config.json", "set the us3 config json file")
+    flag.Parse()
+
+    err := ini.MapTo(config, confFile)
+    if err != nil {
+        fmt.Printf("Fail to read file: %v", err)
+        return
+    }
+    fmt.Println(config)
+
+    if _, err = kafka.Init(config.Kafka.Dsn); err != nil {
+        fmt.Println("init kafka failed, err:%v\n", err)
+        return
+    }
+    fmt.Println("init kafka success.")
+
+    us3.Run(us3ConfFile, kafkaMsg)
+    run()
+}
+
+func run() {
+  
+    for {
+        select {
+        case line := <-kafkaMsg:
+            kafka.SendToKafka(config.Kafka.Topic, line)
+        default:
+            time.Sleep(time.Second)
+        }
+    }
+}
